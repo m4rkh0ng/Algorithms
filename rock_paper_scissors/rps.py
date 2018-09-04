26141224551ab7ad02afb00bdb9a97cb99e61fc9
Mark Hong <m4rkh0ng@gmail.com>
2018-09-04 19:18:06 -0400

    rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,19 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  options = [['rock'], ['paper'], ['scissors']]
+  result = []
+  if n==0:
+    return result
+  if n>0:
+    def recursion(possibilities):
+      if len(possibilities) == n:
+        result.append(possibilities)
+        return
+      for p in options:
+        recursion(possibilities + p)
+    recursion([])
+    return result
 
 if __name__ == "__main__":
   # Test out your implementation from the command line
